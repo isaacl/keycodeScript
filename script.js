d2c8dfb0e6c1df37c27d8d08cb62c27dd61249c0
Jonas <jonsinge@freenet.de>
2017-03-23 11:04:36 +0100

    - allow empty list for blocked keycodes - add list of blocked keycodes when Alt modifier is pressed - add Alt+S to shortcuts blocked by default (used for special characters in german and polish language, but at the same time for sending an email in Outlook Web Access) - use builtin Set() objects for the keycode lookups

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
 // ==UserScript==
 // @name           Disable website keyboard hooks
 // @description    Stop websites from hijacking keyboard shortcuts.
-// @author         Isaac Levy
+// @author         Isaac Levy, Jonas Singe
 // @run-at         document-start
 // @include        *
 // @grant          none
-// @version        0.0.1
+// @version        0.0.2
 // @namespace      https://isaacrlevy.com
 // ==/UserScript==
 
-var keycodes = [ // Add keycodes as desired, keep sorted.
-    37, 38, 39, 40 // Arrow keys.
-]
+// Disable these keys when no keyboard modifier is pressed
+var keycodes = new Set([
+    // Add keycodes as desired
+    37, 38, 39, 40 // Arrow Keys.
+]);
 
-var meta_keycodes = [ // Disable these when meta key is pressed.
-    70
-];
+// Disable these keys when Meta key is pressed.
+var meta_keycodes = new Set([
+	// Add keycodes as desired
+	70 // Ctrl + F
+]);
+
+// Disable these keys when Alt key is pressed.
+var alt_keycodes = new Set([
+	// Add keycodes as desired
+	83 // Alt + S
+]); 
+
 
 // Don't change below this line.
 
 var isMac = navigator.platform.toLowerCase().indexOf('mac') >= 0;
 
-// Create a fast lookup.
-// This saves work during normal typing. Maybe unnecessary.
-var keycode_offset = keycodes[0];
-var keycode_arr = Array(keycodes[keycodes.length - 1] - keycode_offset)
-for (var i = 0, len = keycodes.length; i < len; i++) {
-    keycode_arr[keycodes[i] - keycode_offset] = true;
-}
 
 document.addEventListener('keydown', function(e) {
     //console.log(e);
     if ((isMac && e.metaKey) || (!isMac && e.ctrlKey)) {
-        if (meta_keycodes.indexOf(e.keyCode) >= 0) {
+        if (meta_keycodes.has(e.keyCode)) {
             e.stopImmediatePropagation();
         }
-    } else if (keycode_arr[e.keyCode - keycode_offset]) {
+    } else if (e.altKey) {
+        if (alt_keycodes.has(e.keyCode)) {
+            e.stopImmediatePropagation();
+        }
+    } else if (keycodes.has(e.keyCode)) {
         e.stopImmediatePropagation();
     }
     return false;
